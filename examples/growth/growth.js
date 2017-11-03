e178bfb78fe8ac8b51df9c6010440189e100e77f
Kevin Kelly <kvinklly@gmail.com>
2017-11-02 23:40:09 -0500

    finished draft of growth.js

--- examples/growth/growth.js
+++ examples/growth/growth.js
@@ -1,4 +1,11 @@
-let myCells = [];
+let myCells = [
+  0, 0, 0, 1, 1,
+  0, 1, 1, 1, 0,
+  1, 0, 1, 1, 0,
+  1, 0, 0, 1, 1,
+  0, 1, 1, 0, 0,
+  0, 1, 1, 1, 0
+];
 
 function showArray(arr) {
   var line = "";
@@ -11,9 +18,25 @@ function showArray(arr) {
   }
 }
 
-for (var i = 0; i < 30; i++) {
-  myCells.push(Math.floor(Math.random()*100));
+console.log("=====\nInitial Values:\n=====")
+showArray(myCells);
+
+var blink = function(cell) {
+  return (cell === 1) ? 0 : 1;
 }
 
-console.log("=====\nInitial Values:\n=====")
+console.log("=====\nBlinked Values:\n=====");
+showArray(myCells.map(blink));
+
+var grow = (cell) => {
+  return cell+1;
+}
+
+console.log("=====\nGrown Values:\n=====");
+showArray(myCells.map(grow));
+
+console.log("=====\nIncrement by 2 and Square each Value:\n=====");
+showArray(myCells.map((x) => (x+2) * (x+2)));
+
+console.log("=====\nInitial Values Have Not Changed:\n=====")
 showArray(myCells);
